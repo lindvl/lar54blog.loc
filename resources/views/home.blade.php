4968871c006098691cb6d3eda82e629f382cab35
lindvl <poleznoya@yandex.ru>
2018-12-30 18:08:40 +0500

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+		<div class="col-md-8">
+			<h1 class="text-center">домашняя</h1>
+		</div>
+				<div class="col-md-4">
+			<h4 class="text-center">30 дек 2018</h4>
+		</div>
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                         
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+                <?php 
+                //session_start();
+                //print_r($_SESSION);
+                          // echo "Hello";
+                           ?>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
